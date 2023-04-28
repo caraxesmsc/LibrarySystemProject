339312ae9337769fe3e90f593f89d41aa58d78aa
caraxesmsc <mohamedongo21@gmail.com>
2023-04-28 09:42:45 +0200

    init main

---
+++ LibraryProject/src/main/java/practice/projects/libraryproject/LibraryProject.java
@@ -0,0 +1,10 @@
+
+package practice.projects.libraryproject;
+
+
+public class LibraryProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
