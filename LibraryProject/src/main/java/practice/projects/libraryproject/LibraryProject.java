7e5e45f0ae9cc316f6de388e60e49b2798cc00c6
caraxesmsc <mohamedongo21@gmail.com>
2023-04-28 10:31:54 +0200

    print info function

--- LibraryProject/src/main/java/practice/projects/libraryproject/LibraryProject.java
+++ LibraryProject/src/main/java/practice/projects/libraryproject/LibraryProject.java
@@ -1,10 +1,15 @@
 
 package practice.projects.libraryproject;
-
+import java.util.Scanner;
 
 public class LibraryProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Scanner cin = new Scanner(System.in);
+        Person p1 = new Person(2131,2131,"Reader","Mohamed","Hossam","cairo",01020213746,"mohamed@gmail.com",true);
+        p1.printInfo();
     }
+    
+    
 }
