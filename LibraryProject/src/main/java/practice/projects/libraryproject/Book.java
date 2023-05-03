eb8057a1483a581377a284441910a221af78524f
caraxesmsc <mohamedongo21@gmail.com>
2023-05-03 15:39:30 +0300

    isreturned rentedby and returndate

--- LibraryProject/src/main/java/practice/projects/libraryproject/Book.java
+++ LibraryProject/src/main/java/practice/projects/libraryproject/Book.java
@@ -1,9 +1,38 @@
 package practice.projects.libraryproject;
 
+import java.util.Date;
+
 public class Book {
     private int id;
     private String Title;
     private String Author;
+    private boolean isReturned;
+    private Person rentedBy;
+    private Date returnDate;
+
+    public void setRentedBy(Person rentedBy) {
+        this.rentedBy = rentedBy;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public void setReturned(boolean returned) {
+        isReturned = returned;
+    }
+
+    public Person getRentedBy() {
+        return rentedBy;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public boolean getIsReturned() {
+        return isReturned;
+    }
 
     public Book(int id, String title, String author) {
         this.id = id;
