88b537761d24d75419b1f0b58eba8fd22872ff9c
caraxesmsc <mohamedongo21@gmail.com>
2023-05-03 15:31:59 +0300

    Order now edits 2 dates using 1

--- LibraryProject/src/main/java/practice/projects/libraryproject/Order.java
+++ LibraryProject/src/main/java/practice/projects/libraryproject/Order.java
@@ -5,16 +5,23 @@ import java.time.LocalDateTime;
 public class Order {
     private Person person;
     private Book book;
-    private LocalDateTime date;
+    private LocalDateTime dateOrdered;
+    private LocalDateTime dateReturned;
+
+
     private static int counter;
 
-    public Order(Person person, Book book, LocalDateTime date) {
+    public Order(Person person, Book book, LocalDateTime date,boolean isReturn) {
         this.person = person;
         this.book = book;
-        this.date = date;
-        counter++;
+        if (isReturn) {
+            this.dateReturned = date;
+            counter--;
+        } else {
+            this.dateOrdered = date;
+            counter++;
+        }
     }
-
     public static int getCounter() {
         return counter;
     }
